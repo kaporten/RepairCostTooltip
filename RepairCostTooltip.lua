41e4f6728f144acf5c62c57428bad2f16496d459
porten <porten@gmail.com>
2014-07-11 18:43:29 +0200

    LilVendor plus bugfix
    Bugfix for addon.tRepairableItems being nil in some cases, leading to
    lua erros.

--- RepairCostTooltip.lua
+++ RepairCostTooltip.lua
@@ -5,20 +5,24 @@
  
 require "Window"
 
-local RepairCostTooltip = Apollo.GetPackage("Gemini:Addon-1.0").tPackage:NewAddon("RepairCostTooltip", false, {}, "Gemini:Hook-1.0")
-RepairCostTooltip.ADDON_VERSION = {1, 0, 1}
+local RepairCostTooltip = Apollo.GetPackage("Gemini:Addon-1.0").tPackage:NewAddon("RepairCostTooltip", false, {"Vendor", "LilVendor"}, "Gemini:Hook-1.0")
+RepairCostTooltip.ADDON_VERSION = {2, 0, 0}
 
 function RepairCostTooltip:OnEnable()
-	-- Hook into an appropriate Vendor method 
-	self:Hook(Apollo.GetAddon("Vendor"), "SetBuyButtonText", self.UpdateTooltip)
+	-- Hook into an appropriate Lil/Vendor method 
+	self:Hook(Apollo.GetAddon(self.addonName), "SetBuyButtonText", self.UpdateTooltip)
 end
 
-function RepairCostTooltip:UpdateTooltip()	
-	-- Get ref to buy-button on Vendor (on self, since this is a hooked function)	
-	local buyButton = self.wndVendor:FindChild("Buy")
+function RepairCostTooltip:UpdateTooltip()
+	local addonName = RepairCostTooltip.addonName
+	local addon = Apollo.GetAddon(addonName)
+	local wndVendor = addon["wnd" .. addonName]
+	
+	-- Get ref to buy-button on Vendor (on self, since this is a hooked function)
+	local buyButton = wndVendor:FindChild("Buy")
 	
 	-- If we're not updating to show a "Repair All" scenario, clear tooltip and return
-	if not self.wndVendor:FindChild("VendorTab3"):IsChecked()  	-- Repair tab must be checked on Vendor
+	if not wndVendor:FindChild("VendorTab3"):IsChecked()  	-- Repair tab must be checked on Vendor
 			or buyButton:GetData() then 						-- There must not be item-select data associated with the buy/repair/repair-all btn
 		buyButton:SetTooltip("")
 		return
@@ -26,9 +30,11 @@ function RepairCostTooltip:UpdateTooltip()
 	
 	-- Determine total repair cost
 	local monAmount = 0
-	for _,v in ipairs(self.tRepairableItems) do
-		monAmount = monAmount + v.itemData:GetRepairCost()
-	end 
+	if addon.tRepairableItems ~= nil then
+		for _,v in ipairs(addon.tRepairableItems) do
+			monAmount = monAmount + v.itemData:GetRepairCost()
+		end 
+	end
 
 	-- Produce tooltip window and set amount.
 	local wndTooltip = RepairCostTooltip.ProduceTooltipWindow()
@@ -74,4 +80,15 @@ function RepairCostTooltip:ProduceTooltipWindow()
 
 	-- Create the instance of the window
 	return tWindow:GetInstance()	
+end
+
+function RepairCostTooltip:OnDependencyError()
+	-- One of these should be present (and it should be impossible for both of them to be)
+	if Apollo.GetAddon("Vendor") ~= nil then
+		self.addonName = "Vendor"
+		return true
+	elseif Apollo.GetAddon("LilVendor") ~= nil then
+		self.addonName = "LilVendor"
+		return true
+	end	
 end
